18ac7a861d09f49a51043c3b2cb6c1e41627792e
9stats <55319369+9stats@users.noreply.github.com>
2022-08-30 11:01:45 +0100

    Add files via upload

---
+++ ansible-nginx-new-site/add_new_site.sh
@@ -0,0 +1,14 @@
+read -p 'Existing customer user account to run as: ' customer
+[[ -z "$customer" ]] && { echo "User account empty. Quitting." ; exit 1; }
+[ -z "$(id $customer)" ] && { echo "User does not exist. Quitting." ; exit 1; }
+
+
+read -p 'Domain or Subdomain: ' site
+
+ls /etc/nginx/sites-enabled
+
+read -p "Choose the next number up skiping by 10 example 110 120 : " numb
+
+read -p 'DB name live_examle stag_examle: ' db_name
+
+ansible-playbook -i hosts playbook.yml  -e "site=$site customer=$customer db_name=$db_name numb=$numb"
